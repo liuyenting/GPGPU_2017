4db277189f135193476133e289dcb02ddc7a2fbe
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-02-22 19:07:10 +0800

    Complete the Mario scene

--- lab0/main.cu
+++ lab0/main.cu
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include "SyncedMemory.h"
 
+#define DIVUP(a, b)     ((a+b-1)/b)
+
 #define CHECK {\
 	auto e = cudaDeviceSynchronize();\
 	if (e != cudaSuccess) {\
@@ -14,8 +16,6 @@ const int W = 40;
 const int H = 12;
 
 __global__ void Draw(char *frame) {
-	// TODO: draw more complex things here
-	// Do not just submit the original file provided by the TA!
 	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 	const int x = blockIdx.x * blockDim.x + threadIdx.x;
 	if (y < H and x < W) {
@@ -25,7 +25,28 @@ __global__ void Draw(char *frame) {
 		} else if (y == 0 or y == H-1 or x == 0 or x == W-2) {
 			c = ':';
 		} else {
-			c = ' ';
+			if (y < 5 or x < 8) {
+				c = ' ';
+			} else if (x == 33) {
+				// the pole
+				if (y == H-2) {
+					c = '#';
+				} else {
+					c = '|';
+				}
+			} else if (x == 32 and y == 5) {
+				// the flag
+				c = '<';
+			} else if (x > 7 and x < 22) {
+				// the stairs
+				if (-x/2+14 <= y) {
+					c = '#';
+				} else {
+					c = ' ';
+				}
+			} else {
+				c = ' ';
+			}
 		}
 		frame[y*W+x] = c;
 	}
@@ -37,7 +58,9 @@ int main(int argc, char **argv)
 	auto frame_smem = frame.CreateSync(W*H);
 	CHECK;
 
-	Draw<<<dim3((W-1)/16+1,(H-1)/12+1), dim3(16,12)>>>(frame_smem.get_gpu_wo());
+	dim3 threads(16, 12);
+	dim3 blocks(DIVUP(W, threads.x), (DIVUP(H, threads.y)));
+	Draw<<<blocks, threads>>>(frame_smem.get_gpu_wo());
 	CHECK;
 
 	puts(frame_smem.get_cpu_ro());
