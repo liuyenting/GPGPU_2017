3b4f2f26e186c8536453f873fe3669663e78032d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-03-31 23:41:33 +0800

    Create primitives

--- lab1_indep/engine.cu
+++ lab1_indep/engine.cu
@@ -21,6 +21,38 @@ inline void gpuAssert(
     }
 }
 
+
+struct Ray {
+    float3 orig;    // origin
+    float3 dir;     // direction
+
+    __device__
+    Ray(const float3 _orig, const float3 _dir)
+        : orig(_orig), dir(_dir) {
+    }
+};
+
+/*
+ * Reflection type
+ */
+enum Refl_t {
+    DIFF = 0,   // diffuse
+    SPEC,       // speckle
+    REFR        // refract
+};
+
+struct Object {
+    float3 pos;     // position in world coordinate
+    float3 emi;     // RGB emission
+    float3 color;   // RGB
+    Refl_t refl;    // type of reflection
+
+    __device__
+    virtual bool distance(const Ray &ray) const = 0;
+    __device__
+    virtual bool distance(const Object &obj) const = 0;
+};
+
 int main() {
     const int width = 320, height = 240;
 
