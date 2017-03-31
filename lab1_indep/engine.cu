ef8f1cfc4c7a95246399c3a5d8dff5711b8cbbae
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-03-31 23:44:04 +0800

    Create sphere object

--- lab1_indep/engine.cu
+++ lab1_indep/engine.cu
@@ -1,5 +1,11 @@
-#include <iostream>
+// Library
 #include <cuda_runtime.h>
+
+// Internal
+#include "cutil_math.h"
+
+// System
+#include <iostream>
 
 #define EPS     1e-4f
 #define PI      3.14159265359f
@@ -51,6 +57,37 @@ struct Object {
     virtual bool distance(const Ray &ray) const = 0;
     __device__
     virtual bool distance(const Object &obj) const = 0;
+};
+
+struct Sphere : Object {
+    float rad;  // radius
+
+    __device__
+    virtual bool distance(const Ray &ray) const {
+        /*
+         * ray equation: p(x, y, z) = ray.orig + t*ray.dir
+         * sphere equation: x^2 + y^2 + z^2 = rad^2
+         *
+         * quadratic: ax^2 + bx + c = 0 -> x = (-b +- sqrt(b^2 - 3ac)) / 2a
+         *
+         * solve t^2*ray.dir*ray.dir + 2*t*(orig-p)*ray.dir + (orig-p)*(orig-p) - rad*rad = 0
+         */
+        float3 dist = pos - ray.orig;
+        float b = dot(dist, ray.dir);
+
+        // discriminant
+        float disc = b*b - dot(dist, dist) + rad*rad;
+
+        if (disc < 0) {
+            // ignroe complex solution
+            return 0;
+        } else {
+            disc = sqrtf(disc);
+        }
+        // return the closest point relative the the origin of light ray
+        float t;
+        return ((t = b - disc) > EPS) ? t : ( ((t = b + disc) > EPS) ? t : 0 );
+    }
 };
 
 int main() {
