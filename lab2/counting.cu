410d70c4365e92829c2cfcf8d7885e16df32f9da
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-04-23 16:32:29 +0800

    Transform alphabet and non-alphabet

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -11,7 +11,7 @@ __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
 struct is_alphabet {
-    __host__ __device__
+    __device__
     int operator()(const char c) const {
         return (c != '\n') ? 1 : 0;
     }
@@ -36,6 +36,38 @@ void CountPosition1(const char *text, int *pos, int text_size)
     );
 }
 
+namespace lab2 {
+
+__global__
+void transform(
+    const char *input,
+    int *output,
+    const int n
+) {
+    for (
+        int i = blockIdx.x * blockDim.x + threadIdx.x;
+        i < n;
+        i += blockDim.x * gridDim.x
+    ){
+        output[i] = (input[i] != '\n') ? 1 : 0;
+    }
+}
+
+__global__
+void scan(
+    const int *input,
+    int *output,
+    const int n
+) {
+
+}
+
+}
+
 void CountPosition2(const char *text, int *pos, int text_size)
 {
+    int numSMs;
+    cudaDeviceGetAttribute(&numSMs, cudaDevAttrMultiProcessorCount, 0);
+
+    lab2::transform<<<32*numSMs, 256>>>(text, pos, text_size);
 }
