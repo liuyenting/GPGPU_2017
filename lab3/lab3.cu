9e7cbdfdf8a4f23e828a5bf7e440cea2b127e8b5
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-05-21 16:18:26 +0800

    First trial Implemented using TA's simplified gradient equation.

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -27,70 +27,6 @@ __global__ void SimpleClone(
 	}
 }
 
-__global__ void PoissonClone(
-	const float *background,
-	const float *target,
-	const float *mask,
-	float *output,
-	const int wb, const int hb, const int wt, const int ht,
-	const int oy, const int ox
-) {
-	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
-	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int ct = wt*yt+xt;
-
-	// ignore out-of-range coordinates, 1px spacing
-	if (yt < 1 or yt >= ht-1 or xt < 1 or xt >= wt-1) {
-		return;
-	}
-
-	const int yb = oy+yt, xb = ox+xt;
-	const int cb = wb*yb+xb;
-
-	// calculate target N, S, W, E linear index
-	const int nt = wt*(yt+1)+xt;
-	const int st = wt*(yt-1)+xt;
-	const int wt = wt*yt+(xt-1);
-	const int et = wt*yt+(xt+1);
-	// calculate background N, S, W, E linear index
-	const int nb = wb*(yb+1)+xb;
-	const int sb = wb*(yb-1)+xb;
-	const int wb = wb*yb+(xb+1);
-	const int eb = wb*yb+(xb-1);
-
-	// surrounding pixel sum
-	const float surPx0 = target[nt*3+0] + target[st*3+0] + target[wt*3+0] + target[t*3+0];
-	const float surPx1 = target[nt*3+1] + target[st*3+1] + target[wt*3+1] + target[t*3+1];
-	const float surPx2 = target[nt*3+2] + target[st*3+2] + target[wt*3+2] + target[t*3+2];
-
-	// constant neighbor pixel
-	const float conPx0 = 0.0f, conPx1 = 0.0f, conPx2 = 0.0f;
-	// variate neighbor pixel
-	const float varPx0 = 0.0f, varPx1 = 0.0f, varPx2 = 0.0f;
-	// accumulate the pixels
-	if (mask[nt] > 127.0f) {
-		varPx0 += output[nt*3+0];
-		varPx1 += output[nt*3+1];
-		varPx2 += output[nt*3+2];
-	} else {
-		conPx0 += background[nt*3+0];
-		conPx0 += background[nt*3+1];
-		conPx0 += background[nt*3+2];
-	}
-
-
-
-	if (yt < ht and xt < wt and mask[curt] > 127.0f) {
-		const int yb = oy+yt, xb = ox+xt;
-		const int curb = wb*yb+xb;
-		if (0 <= yb and yb < hb and 0 <= xb and xb < wb) {
-			output[curb*3+0] = target[curt*3+0];
-			output[curb*3+1] = target[curt*3+1];
-			output[curb*3+2] = target[curt*3+2];
-		}
-	}
-}
-
 __global__ void CalculateFixed(
 	const float *background,
 	const float *target,
@@ -99,7 +35,70 @@ __global__ void CalculateFixed(
 	const int wb, const int hb, const int wt, const int ht,
 	const int oy, const int ox
 ) {
+	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+	const int clt = wt*yt+xt;
 
+	// ignore out-of-range pixels
+	if (yt >= ht or xt >= wt) {
+		return;
+	}
+
+	const int yb = oy+yt, xb = ox+xt;
+	const int clb = wb*yb+xb;
+
+	// 1px spacing, using background values
+	if (yt < 1 or yt >= ht-1 or xt < 1 or xt >= wt-1) {
+		fixed[clt*3+0] = background[clb*3+0];
+		fixed[clt*3+1] = background[clb*3+1];
+		fixed[clt*3+2] = background[clb*3+2];
+		return;
+	}
+
+	// calculate target N, S, W, E linear index
+	const int nlt = wt*(yt+1)+xt;
+	const int slt = wt*(yt-1)+xt;
+	const int wlt = wt*yt+(xt-1);
+	const int elt = wt*yt+(xt+1);
+	// calculate background N, S, W, E linear index
+	const int nlb = wb*(yb+1)+xb;
+	const int slb = wb*(yb-1)+xb;
+	const int wlb = wb*yb+(xb+1);
+	const int elb = wb*yb+(xb-1);
+
+	// surrounding pixel sum
+	const float surPx0 = target[nlt*3+0] + target[slt*3+0] + target[wlt*3+0] + target[elt*3+0];
+	const float surPx1 = target[nlt*3+1] + target[slt*3+1] + target[wlt*3+1] + target[elt*3+1];
+	const float surPx2 = target[nlt*3+2] + target[slt*3+2] + target[wlt*3+2] + target[elt*3+2];
+
+	// constant neighbor pixel
+	float conPx0 = 0.0f, conPx1 = 0.0f, conPx2 = 0.0f;
+	// accumulate the background pixels
+	if (mask[nlt] <= 127.0f) {
+		conPx0 += background[nlb*3+0];
+		conPx1 += background[nlb*3+1];
+		conPx2 += background[nlb*3+2];
+	}
+	if (mask[slt] <= 127.0f) {
+		conPx0 += background[slb*3+0];
+		conPx1 += background[slb*3+1];
+		conPx2 += background[slb*3+2];
+	}
+	if (mask[wlt] <= 127.0f) {
+		conPx0 += background[wlb*3+0];
+		conPx1 += background[wlb*3+1];
+		conPx2 += background[wlb*3+2];
+	}
+	if (mask[elt] <= 127.0f) {
+		conPx0 += background[elb*3+0];
+		conPx1 += background[elb*3+1];
+		conPx2 += background[elb*3+2];
+	}
+
+	// fill the constant value
+	fixed[clt*3+0] = (4*target[clt*3+0] - surPx0 + conPx0)/4;
+	fixed[clt*3+1] = (4*target[clt*3+1] - surPx1 + conPx1)/4;
+	fixed[clt*3+2] = (4*target[clt*3+2] - surPx2 + conPx2)/4;
 }
 
 __global__ void PoissonImageCloningIteration(
@@ -109,7 +108,60 @@ __global__ void PoissonImageCloningIteration(
 	float *out,
 	const int wt, const int ht
 ) {
+	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+	const int clt = wt*yt+xt;
 
+	// ignore out-of-range pixels
+	if (yt >= ht or xt >= wt) {
+		return;
+	}
+
+	const int yb = oy+yt, xb = ox+xt;
+	const int clb = wb*yb+xb;
+
+	// 1px spacing, using background values
+	if (yt < 1 or yt >= ht-1 or xt < 1 or xt >= wt-1) {
+		out[clt*3+0] = in[clb*3+0];
+		out[clt*3+1] = in[clb*3+1];
+		out[clt*3+2] = in[clb*3+2];
+		return;
+	}
+
+	// calculate target N, S, W, E linear index
+	const int nlt = wt*(yt+1)+xt;
+	const int slt = wt*(yt-1)+xt;
+	const int wlt = wt*yt+(xt-1);
+	const int elt = wt*yt+(xt+1);
+
+	// constant neighbor pixel
+	float varPx0 = 0.0f, varPx1 = 0.0f, varPx2 = 0.0f;
+	// accumulate the background pixels
+	if (mask[nlt] > 127.0f) {
+		varPx0 += in[nlt*3+0];
+		varPx1 += in[nlt*3+1];
+		varPx2 += in[nlt*3+2];
+	}
+	if (mask[slt] > 127.0f) {
+		varPx0 += in[slt*3+0];
+		varPx1 += in[slt*3+1];
+		varPx2 += in[slt*3+2];
+	}
+	if (mask[wlt] > 127.0f) {
+		varPx0 += in[wlt*3+0];
+		varPx1 += in[wlt*3+1];
+		varPx2 += in[wlt*3+2];
+	}
+	if (mask[elt] > 127.0f) {
+		varPx0 += in[elt*3+0];
+		varPx1 += in[elt*3+1];
+		varPx2 += in[elt*3+2];
+	}
+
+	// fill the result
+	out[clt*3+0] = fixed[clt*3+0] + varPx0/4;
+	out[clt*3+1] = fixed[clt*3+1] + varPx1/4;
+	out[clt*3+2] = fixed[clt*3+2] + varPx2/4;
 }
 
 void PoissonImageCloning(
@@ -136,7 +188,7 @@ void PoissonImageCloning(
 	cudaMemcpy(buf1, target, sizeof(float)*3*wt*ht, cudaMemcpyDeviceToDevice);
 
 	// iterate
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 1; i++) {
 		PoissonImageCloningIteration<<<gdim, bdim>>>(
 			fixed, mask, buf1, buf2, wt, ht
 		);
@@ -148,7 +200,7 @@ void PoissonImageCloning(
 	// copy the image back
 	cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
 	SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
-		background, target, mask, output,
+		background, buf2, mask, output,
 		wb, hb, wt, ht, oy, ox
 	);
 
